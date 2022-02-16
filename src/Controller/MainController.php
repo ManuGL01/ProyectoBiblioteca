e512a735f379636e5ca27a0f7b5a44a209f1c030
Danseh <xxfirox123@gmail.com>
2022-02-16 19:10:18 +0100

    Login en main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,18 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\SecurityController;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+         // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         if($this->isGranted('ROLE_ADMIN'))
         return $this->redirectToRoute('admin', [], Response::HTTP_SEE_OTHER);
         else {
             return $this->render('main/index.html.twig', [
                 'user' => $this->getUser(),
-                
+                'last_username' => $lastUsername, 
+                'error' => $error
             ]);
         }
     }
