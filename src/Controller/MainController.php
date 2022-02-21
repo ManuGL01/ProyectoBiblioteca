bd6df17c1ec210b54a13b44a2b3df1d2e87dac76
Danseh <xxfirox123@gmail.com>
2022-02-21 17:26:58 +0100

    21-02

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,19 +7,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\SecurityController;
 use App\Entity\User;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+         // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         // if($this->isGranted('ROLE_ADMIN'))
-        return $this->redirectToRoute('admin', [], Response::HTTP_SEE_OTHER);
+        // return $this->redirectToRoute('admin', [], Response::HTTP_SEE_OTHER);
         // else {
-        //     return $this->render('main/index.html.twig', [
-        //         'user' => $this->getUser(),
-        //     ]);
+            return $this->render('admin/index.html.twig', [
+                'user' => $this->getUser(),
+                'last_username' => $lastUsername, 
+                'error' => $error
+            ]);
         // }
     }
 }
