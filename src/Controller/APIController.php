ece0dce40ae21f7aec006f927c6b3e539ba7d7ce
jtorof <jtorofdez@gmail.com>
2022-02-23 17:35:46 +0100

    preliminar

---
+++ src/Controller/APIController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ComentarioRepository;
+use App\Repository\LibroRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('api')]
+class APIController extends AbstractController
+{
+    #[Route('/libros', name: 'api_libros')]
+    public function index(LibroRepository $libroRepository):Response
+    {
+        /* $libros = $libroRepository
+            ->findBy(
+                ['hidden' => 0]
+            ); */
+
+        //return $this->json($libros, Response::HTTP_OK, [], ['groups' => 'post1']);
+        return $this->json([
+            'message' => 'OK',
+        ]);
+    }
+
+    #[Route('/login', name: 'api_login')]
+    public function login(#[CurrentUser] ?User $user): Response
+    {
+        return $this->json([
+            'user' => $this->getUser() ? $this->getUser()->getId() : null]
+        );
+    }
+}
