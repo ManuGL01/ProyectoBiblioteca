8243d17d4be1ceeb16c27d8ff6e32dcc21a61215
jtorof <jtorofdez@gmail.com>
2022-02-24 20:21:24 +0100

    login y addcoment

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class APIController extends AbstractController
 {
     #[Route('/libros', name: 'api_libros')]
-    public function index(LibroRepository $libroRepository):Response
+    public function libros(LibroRepository $libroRepository):Response
     {
         $libros = $libroRepository
             ->findAll();
@@ -22,10 +22,37 @@ class APIController extends AbstractController
         return $this->json($libros, Response::HTTP_OK, [], ['groups' => 'infoLibros']);
     }
 
+    #[Route('/addcomment', name: 'api_add_comment')]
+    public function addComment(PostRepository $postRepository, EntityManagerInterface $entityManager, Request $request):Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $postId = $data['postId'];
+        $content = $data['content'];
+        
+        $comment = new Comment();
+        $post = $postRepository
+            ->findOneBy(
+                ['id' => $postId]
+            );
+
+        $comment->setPost($post);
+        $comment->setAuthor($this->getUser());
+        $comment->setHidden(false);
+        $comment->setContent($content);
+
+        $entityManager->persist($comment);
+        $entityManager->flush();
+        
+        return $this->json([
+            'message' => 'OK',
+            'postId' => $postId,
+        ]);
+    }
+
     #[Route('/login', name: 'api_login')]
     public function login(): Response
     {
-        // a jalal le dice que getId() no existe?
+        // a jalal le dice que getId() no existe? oosas del tabnine o de php o algo?
         return $this->json([
             'user' => $this->getUser() ? $this->getUser()->getId() : null]
         );
