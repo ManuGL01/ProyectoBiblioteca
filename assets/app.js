a331436e06807d96894571c8d7e5ebcab914159d
Juande <cloud15jc@gmail.com>
2022-02-28 20:49:27 +0100

    adding routes

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,20 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
 // start the Stimulus application
-import './bootstrap';
+//import '../bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+import Layout from './components/Layout';
+    
+ReactDOM.render(
+    <>
+    {/* <Router><Home /></Router>
+        <Prueba/> */}
+        <Layout/>
+    </>
+    , document.getElementById('adminIndex')
+);
+
---
+++ assets/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <nav class="navbar w-100">
+            <a href=""><img id="logo" src="/img/logo.png" alt="logoHLANZ"/></a>
+            <h1>Biblioteca I.E.S. HLANZ</h1>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="">Ajustes</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Cerrar sesión</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
