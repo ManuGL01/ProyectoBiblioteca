abf4da2d1a845aa34b32cac999b126dd58756d3b
jtorof <jtorofdez@gmail.com>
2022-03-02 18:46:00 +0100

    test CORS

--- assets/components/Main.jsx
+++ assets/components/Main.jsx
@@ -1,32 +1,35 @@
-import React , {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Main = () => {
     const [libros, setLibros] = useState([]);
     useEffect(() => {
         getInfo();
-    },[])
+    }, [])
     const getInfo = async () => {
-        try{
-        const url = `http://localhost:8000/api/libros`;
-        let respuesta = await fetch(url);
-        let data = await respuesta.json();
-        setLibros(data);
-        } catch{
+        try {
+            const url = `http://localhost:8000/api/libros`;
+            let respuesta = await fetch(url, {
+                method: 'GET',
+                mode: 'Access-Control-Allow-Origin',
+            });
+            let data = await respuesta.json();
+            setLibros(data);
+        } catch {
             console.log(error);
         }
     };
-  return (
-    <>
-    <section class = "libros">
-        {libros.map((libro) =>(
-            <div>
-                <h4>Titulo - {libro.titulo}</h4>
-                <p>Autor - {libro.autor}</p>
-            </div>
-        ))}
-    </section>
-    </>
-  )
+    return (
+        <>
+            <section class="libros">
+                {libros.map((libro) => (
+                    <div>
+                        <h4>Titulo - {libro.titulo}</h4>
+                        <p>Autor - {libro.autor}</p>
+                    </div>
+                ))}
+            </section>
+        </>
+    )
 }
 
 export default Main
