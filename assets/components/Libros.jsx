609887214d13eaf934de33be2823c3e2825ca18c
jtorof <78854533+jtorof@users.noreply.github.com>
2022-03-08 20:48:47 +0100

    arreglado?

--- assets/components/Libros.jsx
+++ assets/components/Libros.jsx
@@ -18,29 +18,33 @@ const Libros = () => {
             console.log(error);
         }
     };
-  return (
-    <>
-    
-        <section className="librosYbuscador">
-            
-            <form className="formBuscador">
-                <input type="search" name="inputBuscador" className="form-control" id="inputBuscador"/>
-                <button type="submit" name="btnBuscar" className="btn btn-primary mt-0 ml-2" id="btnBuscar">Buscar</button>
-            </form>
 
-            <section className = "libros">
-                {libros.map((libro) =>(
-                    <div className = "libro" key={libro.id}>
-                        <h4>{libro.titulo}</h4>
-                        <p>{libro.autor}</p>
-                    </div>
-                ))}
+    useEffect(() => {
+        getInfo();
+    }, [])
+    return (
+        <>
+
+            <section className="librosYbuscador">
+
+                <form className="formBuscador">
+                    <input type="search" name="inputBuscador" className="form-control" id="inputBuscador" />
+                    <button type="submit" name="btnBuscar" className="btn btn-primary mt-0 ml-2" id="btnBuscar">Buscar</button>
+                </form>
+
+                <section className="libros">
+                    {libros.map((libro) => (
+                        <div className="libro" key={libro.id}>
+                            <h4>{libro.titulo}</h4>
+                            <p>{libro.autor}</p>
+                        </div>
+                    ))}
+                </section>
+
             </section>
 
-        </section>
-    
-    </>
-  )
+        </>
+    )
 }
 
 export default Libros
