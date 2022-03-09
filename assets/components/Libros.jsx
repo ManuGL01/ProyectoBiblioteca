6bf9ab8dfef9acb681ce7fc7f69364659e4f7094
jtorof <jtorofdez@gmail.com>
2022-03-09 18:30:48 +0100

    arreglado quizás

--- assets/components/Libros.jsx
+++ assets/components/Libros.jsx
@@ -4,78 +4,94 @@ import Buscador from './Buscador';
 
 const url = `http://127.0.0.1:8000/api/libros`;
 
-const Libros = ({userGlobal}) => {
-    const [libros, setLibros] = useState([]);
-    const [paginationInfo, setPaginationInfo] = useState({})
+const Libros = ({ userGlobal }) => {
+  const [libros, setLibros] = useState([]);
+  const [paginationInfo, setPaginationInfo] = useState({});
+  const [multiplePages, setMultiplePages] = useState(false);
 
-    const getInfo = async (url) => {
-        try {            
-            let respuesta = await fetch(url);
-            let data = await respuesta.json();
-            console.log(data);
-            setLibros(data["hydra:member"]);
-            setPaginationInfo(data["hydra:view"]);
-        } catch (error) {
-            console.log(error);
+
+  const getInfo = async (url) => {
+    try {
+      let respuesta = await fetch(url);
+      let data = await respuesta.json();
+      console.log(data);
+      setLibros(data["hydra:member"]);
+      if ("hydra:view" in data) {
+        setPaginationInfo(data["hydra:view"]);
+        setMultiplePages(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNext = () => {
+    getInfo(`http://127.0.0.1:8000${paginationInfo["hydra:next"]}`);
+  }
+
+  const handlePrevious = () => {
+    getInfo(`http://127.0.0.1:8000${paginationInfo["hydra:previous"]}`);
+  }
+
+  useEffect(() => {
+    getInfo(url);
+  }, []);
+
+  return (
+    <>
+      <section className="librosYbuscador">
+
+        <Buscador setLibros={setLibros} />
+
+        {userGlobal?.username ?
+          <section className='librosSinLogin'>
+            {libros.map((libro) => (
+              <Link to={'libro/' + libro.id.toString()} key={libro.id}>
+                <div className="libroSinLoguin" key={libro.id}>
+                  <h4>{libro.titulo}</h4>
+                  <p>{libro.autor}</p>
+                </div>
+              </Link>
+            ))}
+          </section>
+          :
+          <section className='librosConLoguin'>
+            {libros.map((libro) => (
+              <Link to={'libro/' + libro.id.toString()} key={libro.id}>
+                <div className="libroConLoguin" >
+                  <h4>{libro.titulo}</h4>
+                  <p>{libro.autor}</p>
+                </div>
+              </Link>
+            ))}
+          </section>
         }
-    };
+        {/* {
+          paginationInfo["hydra:first"] === paginationInfo["@id"] ?
+            null :
+            <button onClick={handlePrevious} className="btn">Anterior</button>
+        }
+        {
+          paginationInfo["@id"] === paginationInfo["hydra:last"] ?
+            null :
+            <button onClick={handleNext} className="btn">Siguiente</button>
+        } */}
 
-    const handleNext = () => {
-        getInfo(`http://127.0.0.1:8000${paginationInfo["hydra:next"]}`);
-    }
+        {multiplePages ?
+          paginationInfo["hydra:first"] === paginationInfo["@id"] ?
+            null :
+            <button onClick={handlePrevious} className="btn">Anterior</button> :
+          null}
 
-    const handlePrevious = () => {
-        getInfo(`http://127.0.0.1:8000${paginationInfo["hydra:previous"]}`);
-    }
+        {multiplePages ?
+          paginationInfo["@id"] === paginationInfo["hydra:last"] ?
+          null :
+          <button onClick={handleNext} className="btn">Siguiente</button> :
+          null}
 
-    useEffect(() => {
-        getInfo(url);
-    }, []);
-
-    return (
-        <>
-            <section className="librosYbuscador">
-
-            <Buscador setLibros={setLibros}/> 
-
-            {userGlobal?.username ? 
-                <section className='librosSinLogin'>
-                    {libros.map((libro) => (
-                    <Link to={'libro/'+libro.id.toString()} key={libro.id}>
-                        <div className="libroSinLoguin" key={libro.id}>
-                            <h4>{libro.titulo}</h4>
-                            <p>{libro.autor}</p>
-                        </div>
-                    </Link>
-                    ))}
-                </section>
-            : 
-                <section className='librosConLoguin'>
-                    {libros.map((libro) => (
-                    <Link to={'libro/'+libro.id.toString()} key={libro.id}>
-                        <div className="libroConLoguin" >
-                            <h4>{libro.titulo}</h4>
-                            <p>{libro.autor}</p>
-                        </div>
-                    </Link>
-                    ))}
-                </section>
-            }
-            {
-                paginationInfo["hydra:first"] === paginationInfo["@id"] ?
-                null :
-                <button onClick={handlePrevious} className="btn">Anterior</button>
-            }
-            {
-                paginationInfo["@id"] === paginationInfo["hydra:last"] ?
-                null :
-                <button onClick={handleNext} className="btn">Siguiente</button>
-            }
-
-
-            </section>
-        </>
-    )
+      </section>
+    </>
+  )
 }
 
 export default Libros
